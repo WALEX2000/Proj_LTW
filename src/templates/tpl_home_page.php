7a5d54bc6faa21768f381f12a4183c517c21c063
Alex <up201705049@fe.up.pt>
2019-12-03 12:20:16 +0000

    coisas lindas no php

--- src/templates/tpl_home_page.php
+++ src/templates/tpl_home_page.php
@@ -1,19 +1,73 @@
 <?php function draw_home()
 { ?>
     <div id="body">
-                <div id="SpotlightBox">
-                    <div id="Recommendations" class="HousePreviewer">
-                        <h1>Recommendations</h1>
+            <div id="TopHouseFrame">
+                <img id="TopHouse" src="../../images/topHouse.jpg" alt="TopHouse"/>
+                <div class="BottomText">
+                    <p>Nome</p>
+                </div>
+            </div>
+            <div id="SpotlightFrame">
+                <div id="Recommendations" class="HousePreviewer">
+                    <div class="PreviewedHouse">
+                        <img src="../../images/Room1.jpg" class="PreviewedHouseImage"/>
+                        <div class="BottomTextPreview">
+                            <p>Nome</p>
+                        </div>
                     </div>
-                    <div id="Trending" class="HousePreviewer">
-                        <h1>Trending</h1>
+                    <div class="PreviewedHouse">
+                        <img src="../../images/topHouse.jpg" class="PreviewedHouseImage"/>
+                        <div class="BottomTextPreview">
+                            <p>Nome</p>
+                        </div>
+                     </div>
+                     <div class="PreviewedHouse">
+                         <img src="../../images/Room2.jpg" class="PreviewedHouseImage"/>
+                         <div class="BottomTextPreview">
+                             <p>Nome</p>
+                         </div>
                     </div>
-                    <div id="NewAdditions" class="HousePreviewer">
-                        <h1>NewAdditions</h1>
+                </div>
+                <div id="Trending" class="HousePreviewer">
+                    <div class="PreviewedHouse">
+                        <img src="../../images/Room1.jpg" class="PreviewedHouseImage"/>
+                        <div class="BottomTextPreview">
+                            <p>Nome</p>
+                        </div>
+                    </div>
+                    <div class="PreviewedHouse">
+                        <img src="../../images/Room1.jpg" class="PreviewedHouseImage"/>
+                        <div class="BottomTextPreview">
+                            <p>Nome</p>
+                        </div>
+                    </div>
+                    <div class="PreviewedHouse">
+                        <img src="../../images/Room2.jpg" class="PreviewedHouseImage"/>
+                        <div class="BottomTextPreview">
+                            <p>Nome</p>
+                        </div>
+                    </div>
+                </div>
+                <div id="NewAdditions" class="HousePreviewer">
+                    <div class="PreviewedHouse">
+                        <img src="../../images/Room1.jpg" class="PreviewedHouseImage"/>
+                        <div class="BottomTextPreview">
+                            <p>Nome</p>
+                        </div>
+                    </div>
+                    <div class="PreviewedHouse">
+                        <img src="../../images/Room2.jpg" class="PreviewedHouseImage"/>
+                        <div class="BottomTextPreview">
+                            <p>Nome</p>
+                        </div>
+                    </div>
+                    <div class="PreviewedHouse">
+                        <img src="../../images/topHouse.jpg" class="PreviewedHouseImage"/>
+                        <div class="BottomTextPreview">
+                            <p>Nome</p>
+                        </div>
                     </div>
                 </div>
             </div>
-
         </div>
-    </div>
 <?php } ?>
