c523127c6e8d6b749a61a5bfe58a32c9558b7629
Alex <up201705049@fe.up.pt>
2019-12-12 23:08:01 +0000

    scroll das previewed casas

--- src/templates/tpl_home_page.php
+++ src/templates/tpl_home_page.php
@@ -23,13 +23,13 @@
         </div>
         <div id="SpotlightFrame">
             <div id="Recommendations" class="HousePreviewer">
-                <button type="button" class = "arrowButton"><i class="fa fa-chevron-left fa-5x"></i></button>
+                <button type="button" class = "arrowButton" id="recommendedBack"><i class="fa fa-chevron-left fa-5x"></i></button>
                 <?php
                 //TODO change query for each preview
                     foreach ($all_stories as $story) {
                         $image_url = get_image_url($story['main_image']);
                 ?>
-                    <div class="PreviewedHouse">
+                    <div class="PreviewedHouse RecommendedHouse">
                         <a href="story.php?story_id=<?= $story['id'] ?>">
                             <img src="../../images/<?= $image_url ?>" alt="Photo of <?= $story['name'] ?> story" class="PreviewedHouseImage"/>
                         </a>
@@ -40,15 +40,15 @@
                 <?php
                     }
                 ?>
-                <button type="button" class = "arrowButton"><i class="fa fa-chevron-right fa-5x"></i></button>
+                <button type="button" class = "arrowButton" id="recommendedFront"><i class="fa fa-chevron-right fa-5x"></i></button>
             </div>
             <div id="Trending" class="HousePreviewer">
-                <button type="button" class = "arrowButton"><i class="fa fa-chevron-left fa-5x"></i></button>
+                <button type="button" class = "arrowButton" id="trendingBack"><i class="fa fa-chevron-left fa-5x"></i></button>
                 <?php
                     foreach ($all_stories as $story) {
                         $image_url = get_image_url($story['main_image']);
                 ?>
-                    <div class="PreviewedHouse">
+                    <div class="PreviewedHouse TrendingHouse">
                         <a href="story.php?story_id=<?= $story['id'] ?>">
                             <img src="../../images/<?= $image_url ?>" alt="Photo of <?= $story['name'] ?> story" class="PreviewedHouseImage"/>
                         </a>
@@ -59,15 +59,15 @@
                 <?php
                     }
                 ?>
-                <button type="button" class = "arrowButton"><i class="fa fa-chevron-right fa-5x"></i></button>
+                <button type="button" class = "arrowButton" id="trendingFront"><i class="fa fa-chevron-right fa-5x"></i></button>
             </div>
             <div id="NewAdditions" class="HousePreviewer">
-                <button type="button" class = "arrowButton"><i class="fa fa-chevron-left fa-5x"></i></button>
+                <button type="button" class = "arrowButton" id="newBack"><i class="fa fa-chevron-left fa-5x"></i></button>
                 <?php
                     foreach ($all_stories as $story) {
                         $image_url = get_image_url($story['main_image']);
                 ?>
-                    <div class="PreviewedHouse">
+                    <div class="PreviewedHouse NewHouse">
                         <a href="story.php?story_id=<?= $story['id'] ?>">
                             <img src="../../images/<?= $image_url ?>" alt="Photo of <?= $story['name'] ?> story" class="PreviewedHouseImage"/>
                         </a>
@@ -78,7 +78,7 @@
                 <?php
                     }
                 ?>
-                <button type="button" class = "arrowButton"><i class="fa fa-chevron-right fa-5x"></i></button>
+                <button type="button" class = "arrowButton" id="newFront"><i class="fa fa-chevron-right fa-5x"></i></button>
             </div>
         </div>
     </div>
